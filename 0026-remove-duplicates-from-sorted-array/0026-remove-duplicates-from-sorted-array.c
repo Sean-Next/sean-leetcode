eebccea876b429efb003d641b5d8d88fd9d38686
Sean-Next <sean.wang.official@gmail.com>
2026-05-08 11:13:49 +0800

    Time: 0 ms (100%), Space: 12.6 MB (69.24%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,16 @@
+int removeDuplicates(int* nums, int numsSize) 
+{
+    int a = 0;
+    int b = 0;
+
+    while (b < numsSize)
+    {
+        if (nums[a] != nums[b])
+        {
+            nums[++a] = nums[b];
+        }
+        b++;
+    }
+
+    return a + 1;
+}
