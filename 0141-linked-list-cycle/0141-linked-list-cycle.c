1733fad59de9a28ddd71c04edf405e7d41436b26
Sean-Next <sean.wang.official@gmail.com>
2026-05-08 11:15:19 +0800

    Time: 18 ms (12.64%), Space: 11.7 MB (72.3%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode ListNode;
+bool hasCycle(struct ListNode *head) 
+{
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
